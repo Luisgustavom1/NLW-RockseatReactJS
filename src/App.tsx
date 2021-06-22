b406e1b6c9e16dcc167e76e0668fa91f82131f03
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-22 20:09:46 -0300

    autenticação pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-// import {Home} from './pages/Home'
+import {BrowserRouter, Route} from 'react-router-dom'
 
+import {Home} from './pages/Home'
 import {NewRoom} from './pages/newRoom'
+
+import AuthContextProvider from './contexts/authContext'
 
 function App() {
   return (
-    <NewRoom/>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home}/> 
+        <Route path="/room/new" component={NewRoom}/>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
